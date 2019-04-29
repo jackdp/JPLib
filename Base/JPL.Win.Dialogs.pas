40e3b303d43a37e2e55db82ed38d02322aa4e4ac
jacek <jacekpazera@wp.pl>
2019-04-30 01:00:13 +0200

    [New Unit] JPL.Win.Dialogs

---
+++ Base/JPL.Win.Dialogs.pas
@@ -0,0 +1,52 @@
+unit JPL.Win.Dialogs;
+
+interface
+
+uses
+  Windows;
+
+{
+  Information = Asterisks
+  Warning = Exclamation
+  Error = Hand = Stop
+}
+
+procedure WinMsg(const Text, Caption: string; Handle: HWND = 0; MBType: DWORD = MB_OK or MB_ICONINFORMATION);
+procedure MB(const Text: string; Caption: string = 'Information'; Handle: HWND = 0);
+
+procedure WinMsgInfo(Text: string; Caption: string = 'Information'; Handle: HWND = 0);
+procedure WinMsgWarning(Text: string; Caption: string = 'Warning'; Handle: HWND = 0);
+procedure WinMsgError(Text: string; Caption: string = 'Error'; Handle: HWND = 0);
+
+
+implementation
+
+
+
+procedure WinMsg(const Text, Caption: string; Handle: HWND = 0; MBType: DWORD = MB_OK or MB_ICONINFORMATION);
+begin
+  MessageBox(Handle, PChar(Text), PChar(Caption), MBType);
+end;
+
+procedure MB(const Text: string; Caption: string = 'Information'; Handle: HWND = 0);
+begin
+  WinMsgInfo(Text, Caption, Handle);
+end;
+
+procedure WinMsgInfo(Text: string; Caption: string = 'Information'; Handle: HWND = 0);
+begin
+  WinMsg(Text, Caption, Handle, MB_OK or MB_ICONINFORMATION);
+end;
+
+procedure WinMsgWarning(Text: string; Caption: string = 'Warning'; Handle: HWND = 0);
+begin
+  WinMsg(Text, Caption, Handle, MB_OK or MB_ICONWARNING);
+end;
+
+procedure WinMsgError(Text: string; Caption: string = 'Error'; Handle: HWND = 0);
+begin
+  WinMsg(Text, Caption, Handle, MB_OK or MB_ICONERROR);
+end;
+
+
+end.
