7100d078097ad05e1569e637301847c09130ff75
Jacek <jacekpazera@wp.pl>
2020-08-22 00:46:19 +0200

    Utils: +func. NumberIn

--- Base/JPL.Utils.pas
+++ Base/JPL.Utils.pas
@@ -1,6 +1,15 @@
 unit JPL.Utils;
 
+{
+  Jacek Pazera
+  http://www.pazera-software.com
+}
+
+{$I .\..\jp.inc}
+{$IFDEF FPC}{$mode delphi}{$ENDIF}
+
 interface
+
 
 
 function IfThenStr(BoolValue: Boolean; const ResultIfTrue: string; ResultIfFalse: string = ''): string;
@@ -10,7 +19,15 @@ function IfThenInt64(BoolValue: Boolean; const ResultIfTrue: Int64; ResultIfFals
 function IfThenUInt64(BoolValue: Boolean; const ResultIfTrue: UInt64; ResultIfFalse: UInt64 = 0): UInt64;
 function IfThenFloat(BoolValue: Boolean; const ResultIfTrue: Double; ResultIfFalse: Double = -1): Double;
 
+function NumberIn(const Number: integer; IntValArray: array of integer): Boolean; overload;
+function NumberIn(const Number: Int64; Int64ValArray: array of integer): Boolean; overload;
+function NumberIn(const Number: Cardinal; CardinalValArray: array of Cardinal): Boolean; overload;
+function NumberIn(const Number: Double; DoubleValArray: array of Double): Boolean; overload;
+
+
+
 implementation
+
 
 
 function IfThenStr(BoolValue: Boolean; const ResultIfTrue: string; ResultIfFalse: string = ''): string;
@@ -43,5 +60,60 @@ function IfThenFloat(BoolValue: Boolean; const ResultIfTrue: Double; ResultIfFal
 begin
   if BoolValue then Result := ResultIfTrue else Result := ResultIfFalse;
 end;
+
+function NumberIn(const Number: integer; IntValArray: array of integer): Boolean;
+var
+  i: integer;
+begin
+  Result := False;
+  for i := 0 to Length(IntValArray) - 1 do
+    if IntValArray[i] = Number then
+    begin
+      Result := True;
+      Break;
+    end;
+end;
+
+function NumberIn(const Number: Int64; Int64ValArray: array of integer): Boolean;
+var
+  i: integer;
+begin
+  Result := False;
+  for i := 0 to Length(Int64ValArray) - 1 do
+    if Int64ValArray[i] = Number then
+    begin
+      Result := True;
+      Break;
+    end;
+end;
+
+function NumberIn(const Number: Cardinal; CardinalValArray: array of Cardinal): Boolean;
+var
+  i: integer;
+begin
+  Result := False;
+  for i := 0 to Length(CardinalValArray) - 1 do
+    if CardinalValArray[i] = Number then
+    begin
+      Result := True;
+      Break;
+    end;
+end;
+
+function NumberIn(const Number: Double; DoubleValArray: array of Double): Boolean;
+var
+  i: integer;
+begin
+  Result := False;
+  for i := 0 to Length(DoubleValArray) - 1 do
+    if DoubleValArray[i] = Number then
+    begin
+      Result := True;
+      Break;
+    end;
+end;
+
+
+
 
 end.
