bb5d05e9586f32c0cb096a32ccfb902a74d0b76f
Jacek <jacekpazera@wp.pl>
2020-08-30 18:41:49 +0200

    Updated packages

--- packages/CodeTyphon/jplib.pas
+++ packages/CodeTyphon/jplib.pas
@@ -12,7 +12,7 @@ uses
   JPL.FileSearch, JPL.FileSearcher, JPL.IniFile, JPL.JsonHelpers, JPL.LangMgr, JPL.Language, JPL.Math, JPL.MemIniFile, JPL.RTTI, 
   JPL.StrHash, JPL.Strings.Ext, JPL.Strings, JPL.Units, JPL.Utils, JPL.Win.Dialogs, JPL.Win.FileSystem, JPL.Win.Processes, 
   JPL.Win.Shortcuts, JPL.Win.System, JPL.Win.VersionInfo, JPL.StrList, JPL.Win.FileIcons, JPL.ColorArrays, JPL.Colors.ColorClass, 
-  JPL.Colors.List, JPL.Colors, JPL.SimpleLogger, JPL.TimeLogger, TyphonPackageIntf;
+  JPL.Colors.List, JPL.Colors, JPL.SimpleLogger, JPL.TimeLogger, JPL.TStr, JPL.TStringsHelper, JPL.Win.SimpleTimer, TyphonPackageIntf;
 
 implementation
 
