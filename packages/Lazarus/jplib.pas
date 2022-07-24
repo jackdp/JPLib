dfff683794cb8a2fa2920c3c4b101e922d3c5317
Jacek Pazera <jacekpazera@wp.pl>
2022-07-24 07:44:40 +0200

    Updated Lazarus package (+Containers.MultiValueList, Delphi.Info)

--- packages/Lazarus/jplib.pas
+++ packages/Lazarus/jplib.pas
@@ -10,10 +10,11 @@ interface
 uses
   JPL.CmdLineParser, JPL.Console.ColorParser, JPL.Console, JPL.ConsoleApp, JPL.Conversion, JPL.DateTime, JPL.Dialogs, JPL.Files, JPL.FileSearch, 
   JPL.FileSearcher, JPL.IniFile, JPL.JsonHelpers, JPL.Language, JPL.Math, JPL.MemIniFile, JPL.RTTI, JPL.StrHash, JPL.Strings.Ext, JPL.Strings, 
-  JPL.Units, JPL.Utils, JPL.Win.Dialogs, JPL.Win.FileSystem, JPL.Win.Processes, JPL.Win.Shortcuts, JPL.Win.System, JPL.Win.VersionInfo, JPL.StrList, 
-  JPL.Win.FileIcons, JPL.ColorArrays, JPL.Colors.ColorClass, JPL.Colors.List, JPL.Colors, JPL.SimpleLogger, JPL.TimeLogger, JPL.TStr, 
-  JPL.TStringsHelper, JPL.Win.SimpleTimer, JPL.Rects, JPL.ArrayIniFile, JPL.Bytes, JPL.Win.Registry, JPL.LangMgr, JPL.SimpleZip, 
-  JPL.RoseDiag.BinDataList, JPL.RoseDiag.DataFile, JPL.RoseDiag.Diagram, JPL.PixelConv, JPL.Win.Mouse, LazarusPackageIntf;
+  JPL.Units, JPL.Utils, JPL.Win.Dialogs, JPL.Win.FileSystem, JPL.Win.Processes, JPL.Win.Shortcuts, JPL.Win.System, JPL.Win.VersionInfo, 
+  JPL.Win.Mouse, JPL.Containers.MultiValueList, JPL.DefinitionList, JPL.StrList, JPL.Win.FileIcons, JPL.ColorArrays, JPL.Colors.ColorClass, 
+  JPL.Colors.List, JPL.Colors, JPL.SimpleLogger, JPL.TimeLogger, JPL.TStr, JPL.TStringsHelper, JPL.Win.SimpleTimer, JPL.Rects, JPL.ArrayIniFile, 
+  JPL.Bytes, JPL.Win.Registry, JPL.LangMgr, JPL.SimpleZip, JPL.RoseDiag.BinDataList, JPL.RoseDiag.DataFile, JPL.RoseDiag.Diagram, JPL.PixelConv, 
+  JPL.Delphi.Info, LazarusPackageIntf;
 
 implementation
 
